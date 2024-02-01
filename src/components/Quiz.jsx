ca2b09af408b3b27e0ba1625be23ec8366191b64
veersen2001 <veerbahadursen@gmail.com>
2024-02-01 11:16:42 +0530

    My Quiz application

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,15 @@
+
+export default function Quiz() {
+  return (
+    <div className="trivia">
+        <div className="question">what</div>
+        <div className="answers">
+             <div className="answer">hello1</div>
+             <div className="answer">hello2</div>
+             <div className="answer">hello3</div>
+             <div className="answer">hello3</div>
+
+        </div>
+    </div>
+  )
+}
